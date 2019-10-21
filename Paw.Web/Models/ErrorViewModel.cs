81583a31fcd8c64199d71c0102deeb4910eaf1e0
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2019-10-21 17:16:08 -0500

    Creacion de proyecto asp.net core 2.1

---
+++ Paw.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Paw.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
