7b8267614aafd35d411cdeb227e5dbf55d8dfdca
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2019-11-06 16:45:40 -0600

    creación de API, creación de proyecto Xamarin

---
+++ Paw.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿namespace Paw.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Paw.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController:Controller
+    {
+        private readonly DataContext dataContext;
+
+        public ProductsController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            //TODO
+            return Ok(this.dataContext.Products);
+        }
+    }
+}
